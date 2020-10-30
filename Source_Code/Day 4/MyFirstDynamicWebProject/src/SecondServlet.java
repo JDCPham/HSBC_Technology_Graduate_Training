aa1cbe8d1b19cb476bfdc5341e22b1018be64719
--replace-all <john.pham@finatext.com>
2020-10-30 16:13:25 +0000

    Day 4

---
+++ Source_Code/Day 4/MyFirstDynamicWebProject/src/SecondServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SecondServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.write("<h1>Welcome to HSBC<h1>Sent from Second Servlet.");
+		out.close();
+	}
+
+}
